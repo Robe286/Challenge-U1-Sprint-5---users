a3978df3d196d62bc1d0d537d25c07aed27fb29f
Roberto Blasco~ <roberza2202@gmail.com>
2025-03-24 01:41:31 +0100

    response-addelements

---
+++ js/script.js
@@ -0,0 +1,49 @@
+
+// obtener datos simulados de usuarios desde la [API JSONPlaceholder](https://jsonplaceholder.typicode.com/users)
+
+// se agrega una edad aleatoria a cada usuario
+//  - Crea un nuevo array con el objeto y con los nuevos datos a añadir (age, img, address con los nuevos datos) -- Spread operator
+
+// Cada usuario tendrá una imagen asociada por `ID` (están en la carpeta assets/img) son extensión `.jpeg`
+
+// Muestra detalles específicos de cada usuario en la lista en el DOM: name, age, username, img, phone, email, company, address
+
+// address tendrá estos datos como valor: usuario.address.street, usuario.address.suite, usuario.address.city
+
+/*const [user1, ...rest] = users;
+console.log(user1)
+console.log(rest);*/
+
+
+
+const listaUsuarios = document.getElementById('listaUsuarios');
+console.log(listaUsuarios)
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((data) => {
+    
+        data.forEach((user) => {
+            listaUsuarios.innerHTML +=`
+            <li>
+                <div class:"userdata">
+                    <p>Nombre: ${user.name} </p>
+                    <p>Edad: </p>
+                    <p>Username: ${user.username}</p>
+                    <p>Phone: ${user.phone}</p>
+                    <p>Email: ${user.email}</p>
+                </div>
+                <div class="image">
+                    <img src:"./assets/img/1.jpeg" alt=${user.name}
+                </div>
+                <div class="work">
+                    <p>Compañia: ${user.company.name}</p>
+                    <p>Dirección: ${user.address.street}, ${user.address.suite}, ${user.address.city}</p>
+                </div>
+            </li>
+            
+            `
+               
+       }) 
+    });
